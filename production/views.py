74cf8ab2e658042c1b8d22d54f4a0487b6a428c4
satamame <satax@nifty.com>
2019-08-15 22:38:21 +0900

    Updating templates and css for messages.

--- production/views.py
+++ production/views.py
@@ -14,24 +14,17 @@ class ProdCreate(CreateView):
 
     model = Production
     fields = ("name", )
+    template_name_suffix = '_create'
     success_url = reverse_lazy('production:prod_list')
-
-    def get_context_data(self, **kwargs):
-        ''' prod_form.html に渡す context
-        '''
-        context = super().get_context_data(**kwargs)
-        context['form_title'] = '新規公演'
-        context['submit_label'] = '作成'
-        return context
 
     def form_valid(self, form):
         ''' バリデーションを通った時
         '''
-        messages.success(self.request, form.instance.name + " を保存しました")
+        messages.success(self.request, form.instance.name + " を作成しました。")
         return super().form_valid(form)
 
     def form_invalid(self, form):
         ''' バリデーションに失敗した時
         '''
-        messages.warning(self.request, "保存できませんでした")
+        messages.warning(self.request, "作成できませんでした。")
         return super().form_invalid(form)
